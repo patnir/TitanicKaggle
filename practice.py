ec44d7ecb554061ed852156281ba76bf93ec85c6
patnir <rahul20patni@gmail.com>
2017-02-12 18:26:30 -0500

    changes

--- practice.py
+++ practice.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 def read_file():
     with open("test.csv") as f:
         contents = f.readlines()[2:]
     passengerInfoMap = [line.strip().split(',') for line in contents]
+
     return passengerInfoMap
 
 def main():
