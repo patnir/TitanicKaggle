9bb26e3d5af7557f003c18a56ffa630a2c5c50c7
patnir <rahul20patni@gmail.com>
2017-02-12 15:51:10 -0500

    reading files and understanding format

---
+++ practice.py
@@ -0,0 +1,13 @@
+def read_file():
+    with open("test.csv") as f:
+        contents = f.readlines()[2:]
+    passengerInfoMap = [line.strip().split(',') for line in contents]
+    return passengerInfoMap
+
+def main():
+    passengerMapInfo = read_file()
+    print passengerMapInfo
+    return
+
+if __name__ == "__main__":
+    main()
